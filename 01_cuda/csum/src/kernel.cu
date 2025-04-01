cc9d00648714e4086791a51d4340547e5fd125a4
Rodrigo Loro Schuller <rodrigo.loro@impa.br>
2025-04-01 11:15:21 -0300

    (WIP) Add csum

---
+++ 01_cuda/csum/src/kernel.cu
@@ -0,0 +1,27 @@
+#include "kernel.h"
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+#define BLOCK_SIZE 32
+
+__global__ void kernel() {
+
+}
+
+// Define a wrapper function, which launches the kernel
+void kernel_wrapper() {
+    vector<float> v(32);
+    for (int i=0; i<32; i++) {
+        v[i] = i;
+    }
+
+    float* x;
+    cudaMalloc(&x, v.size()*sizeof(v[0]));
+
+    cudaMemcpy(x, v.data(), n*sizeof(v[0]), cudaMemcpyHostToDevice);
+
+    // Launch kernel with <<<block, thread>>> syntax
+    kernel<<<1,32>>>();
+}
