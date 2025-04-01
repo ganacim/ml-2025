91d5158aeeebce6a4cec28acb6f402cb2f0d4ee1
Humberto Seghetto <hseghetto@gmail.com>
2025-03-31 23:25:28 -0300

    Mandelbrot GPU test

--- 01_cuda/mandelbrot/kernel.h
+++ 01_cuda/mandelbrot/kernel.h
@@ -1,6 +1,6 @@
 #ifndef _KERNEL_H_
 #define _KERNEL_H_
 
-void kernel_wrapper();
+void kernel_wrapper(int* result, const int WIDTH, const int HEIGHT, const float scale, const float cx, const float cy);
 
 #endif
