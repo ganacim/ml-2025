8d3121dab9d1b2550dc1e34aafa921a8931f3ef3
Otavio Moreira <otavio.dittrich@impa.br>
2025-04-02 15:19:49 -0300

    Add CPU calculations

--- 01_cuda/mandelbrot_cpu/src/kernel.cu
+++ 01_cuda/mandelbrot_cpu/src/kernel.cu
@@ -8,12 +8,8 @@ using namespace std;
 
 // REFERENCE:
 // https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set#Optimized_escape_time_algorithms
-__global__ void mandelbrot(unsigned char *image, int max_iter, int height,
-                           int width, float xmin, float xmax, float ymin,
-                           float ymax) {
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  int j = blockIdx.y * blockDim.y + threadIdx.y;
-
+void mandelbrot(int i, int j, unsigned char *image, int max_iter, int height,
+                int width, float xmin, float xmax, float ymin, float ymax) {
   if (i >= width or j >= height) {
     return;
   }
@@ -40,7 +36,6 @@ __global__ void mandelbrot(unsigned char *image, int max_iter, int height,
   image[index] = color;
   image[index + 1] = color;
   image[index + 2] = color;
-  __syncthreads();
 }
 
 // Reference: ChatGPT
@@ -55,32 +50,27 @@ void save_image(const string &filename, unsigned char *image, int width,
 // Define a wrapper function, which launches the kernel
 void kernel_wrapper() {
   int height = 2048;
-  int block_size = 64;
   int width = height;
 
   float xmin = -2.0, xmax = 2.0, ymin = -2.0, ymax = 2.0;
 
+  int block_size = 32;
   int max_iter = 10000;
 
   dim3 grid((height + block_size - 1) / block_size,
             (width + block_size - 1) / block_size);
   dim3 block(block_size, block_size);
 
-  unsigned char *image;
-  cudaMalloc(&image, height * width * sizeof(unsigned char) * 3);
+  vector<unsigned char> image(height * width * 3);
 
-  // Launch kernel with <<<block, thread>>> syntax
-  mandelbrot<<<grid, block>>>(image, max_iter, height, width, xmin, xmax, ymin,
-                              ymax);
-
+  int i, j;
+  for (i = 0; i < width; i++)
+    for (j = 0; j < height; j++) {
+      mandelbrot(i, j, image.data(), max_iter, height, width, xmin, xmax, ymin,
+                 ymax);
+    }
   // Save Image
-  vector<unsigned char> host_image(height * width * 3);
 
-  cudaMemcpy(host_image.data(), image,
-             height * width * sizeof(unsigned char) * 3,
-             cudaMemcpyDeviceToHost);
-  cudaFree(image);
-
-  save_image("mandelbrot.ppm", host_image.data(), width, height);
+  save_image("mandelbrot.ppm", image.data(), width, height);
   cout << "Image saved to mandelbrot.ppm" << endl;
 }
---
+++ 01_cuda/mandelbrot_cpu/src/kernel.h
@@ -0,0 +1,7 @@
+#ifndef _KERNEL_H_
+#define _KERNEL_H_
+
+void kernel_wrapper();
+void save_image();
+
+#endif
