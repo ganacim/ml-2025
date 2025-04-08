9f9a4f52ea868629c659062b3e4bd6bce2b81dca
Otavio Moreira <otavio.dittrich@impa.br>
2025-04-08 12:56:08 -0300

    (WP) Started cats and dogs model

--- mlc/model/cats_and_dogs/model.py
+++ mlc/model/cats_and_dogs/model.py
@@ -3,7 +3,7 @@ import torch.optim as optim
 from torch import nn
 from torchsummary import summary
 
-from ..basemodel import BaseModel
+from mlc.model.basemodel import BaseModel
 
 
 class CatsAndDogs(BaseModel):
@@ -14,20 +14,17 @@ class CatsAndDogs(BaseModel):
 
         # keep this here for clarity
         num_classes = args["num_classes"]
-        hidden_dims = args["hidden_dims"]
-        dropout_rate = args["dropout_rate"]
 
-        layers = []
-        prev_dim = 2  # input dimension
-        for hidden_dim in hidden_dims:
-            layers.append(nn.Linear(prev_dim, hidden_dim))
-            layers.append(nn.ReLU())
-            layers.append(nn.BatchNorm1d(hidden_dim))
-            layers.append(nn.Dropout(dropout_rate))
-            prev_dim = hidden_dim
+        conv_layers = []
+        input_num_channels = 3 
+        self.conv1 = nn.Conv2d(input_num_channels, 2*input_num_channels, kernel_size=3, stride=1, padding=1)
+        self.max_pool_1 = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(2*input_num_channels, 4*input_num_channels, kernel_size=3, stride=1, padding=1)
+        self.max_pool_1 = nn.MaxPool2d(2,2)
 
-        layers.append(nn.Linear(prev_dim, num_classes))
-        self.layers = nn.Sequential(*layers)
+
+
+        self.conv_layers = nn.Sequential(*conv_layers)
 
     @staticmethod
     def add_arguments(parser):
@@ -45,14 +42,26 @@ class CatsAndDogs(BaseModel):
         return F.cross_entropy(Y_pred, Y)
 
     def forward(self, x):
-        return self.layers(x)
+        x_out = self.conv1(x)
+        x_out = self.max_pool_1(x_out)
+        x_out = self.conv2(x_out)
+        x_out = self.max_pool_1(x_out)
+        x_out = x_out.view(x_out.size(0), -1)
+        x_out = nn.Linear(x_out.size(1), 2)(x_out)
+        return x_out
+
+
 
     def pre_epoch_hook(self, context):
         print("pre_epoch_hook")
 
 
 def test(args):
-    # create SpiralParameterized model
-    model = SpiralParameterized({"num_classes": 3, "hidden_dims": [100, 10], "dropout_rate": 0.0})
+    # create CatsAndDogs model
+    model = CatsAndDogs({"num_classes": 2})
     # create model summary
-    summary(model, input_size=(2,), device="cpu")
+    summary(model, input_size=(3,224,224), device="cpu")
+
+if __name__ == "__main__":
+    # Test the model
+    test({})
