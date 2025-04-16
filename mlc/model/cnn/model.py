00a4671fd8a64584b76b3c89520bbc73aa87fd63
Humberto <hseghetto@gmail.com>
2025-04-16 19:51:21 +0000

    Adding batchnorm and more parameters

--- mlc/model/cnn/model.py
+++ mlc/model/cnn/model.py
@@ -15,18 +15,25 @@ class CNN(BaseModel):
         # keep this here for clarity
         num_classes = args["num_classes"]
         num_blocks = args["num_blocks"]
+        max_pool = args["max_pool"]
+        batch_norm = args["batch_norm"]
 
         layers = []
 
-        prev_dim = 32  # output dim of the first conv layer
+        prev_dim = args["channels"]  # output dim of the first conv layer
         layers.append(nn.Conv2d(3, prev_dim, 5))
         for _ in range(num_blocks):
             hidden_dim = prev_dim * 2
             layers.append(nn.Conv2d(prev_dim, prev_dim, 5))
             layers.append(nn.ReLU())
-            layers.append(nn.MaxPool2d(2, 2))
+            if batch_norm:
+                layers.append(nn.BatchNorm2d(prev_dim))
+            if max_pool:
+                layers.append(nn.MaxPool2d(2, 2))
             layers.append(nn.Conv2d(prev_dim, hidden_dim, 5))
             layers.append(nn.ReLU())
+            if batch_norm:
+                layers.append(nn.BatchNorm2d(hidden_dim))
 
             prev_dim = hidden_dim
 
@@ -36,9 +43,9 @@ class CNN(BaseModel):
         layers.append(nn.ReLU())
         layers.append(nn.Conv2d(prev_dim, num_classes, 1))
         layers.append(nn.Sigmoid())
-        #layers.append(nn.Linear(16 * 5 * 5, 120))
-        #layers.append(nn.ReLU())
-        #layers.append(nn.Linear(120, num_classes))
+        # layers.append(nn.Linear(16 * 5 * 5, 120))
+        # layers.append(nn.ReLU())
+        # layers.append(nn.Linear(120, num_classes))
 
         self.layers = nn.Sequential(*layers)
 
@@ -46,6 +53,9 @@ class CNN(BaseModel):
     def add_arguments(parser):
         parser.add_argument("--num-classes", type=int, default=1)
         parser.add_argument("--num-blocks", type=int, default=2)
+        parser.add_argument("--max_pool", type=bool, default=True)
+        parser.add_argument("--batch_norm", type=bool, default=True)
+        parser.add_argument("--channels", type=int, default=16)
 
     def get_optimizer(self, learning_rate):
         return optim.Adam(self.parameters(), lr=learning_rate)
@@ -65,6 +75,6 @@ class CNN(BaseModel):
 
 def test(args):
     # create CNN model
-    model = CNN({"num_classes": 1, "num_blocks": 2})
+    model = CNN({"num_classes": 1, "num_blocks": 2, "max_pool": True, "batch_norm": True})
     # create model summary
-    summary(model, input_size=(3,250,250), device="cpu")
+    summary(model, input_size=(3, 250, 250), device="cpu")
