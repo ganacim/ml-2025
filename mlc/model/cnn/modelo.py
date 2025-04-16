465fe544dc0179e4f0be068ec1a692cc3028bcfd
kauan <kauancdmoraes1@gmail.com>
2025-04-16 19:48:24 +0000

    float na loss

--- mlc/model/cnn/modelo.py
+++ mlc/model/cnn/modelo.py
@@ -57,8 +57,8 @@ class CNN(BaseModel):
 
     def evaluate_loss(self, Y_pred, Y):
         # F.cross_entropy expects logits, not probabilities
-        Y_pred = Y_pred.view(-1, 1)
-        Y = Y.view(-1, 1)
+        Y_pred = Y_pred.float().view(-1)
+        Y = Y.float().view(-1)
         return F.binary_cross_entropy(Y_pred, Y)
 
     def forward(self, x):
