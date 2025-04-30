d0916799d47e0610809d1ef328732251ef26c7d5
Francisco Ganacim <ganacim@impa.br>
2025-04-30 14:11:15 -0300

    Add CelebaAlign dataset and splitting functionality with argument parsing

---
+++ mlc/data/celeba_align/dataset.py
@@ -0,0 +1,86 @@
+import argparse
+
+import torch
+from PIL import Image
+from torchvision.transforms import v2
+
+from ...util.resources import data_path
+from ..basedataset import BaseDataset
+
+
+class CelebaAlign(BaseDataset):
+    class DataFold(torch.utils.data.Dataset):
+        def __init__(self, fold_name, scale=256):
+            super().__init__()
+            self.scale = scale
+            self._data_path = data_path("celeba_align")
+            # read files
+            fold_index = self._data_path / f"{fold_name}.txt"
+            with open(fold_index, "r") as f:
+                self.files = f.read().splitlines()
+
+            self.xform = v2.Compose(
+                [
+                    v2.PILToTensor(),
+                    v2.ToDtype(torch.float32, scale=True),  # to [0, 1]
+                    v2.Resize((self.scale, self.scale)),
+                ]
+            )
+
+        def __len__(self):
+            return len(self.files)
+
+        def __getitem__(self, idx):
+            # open image with PIL
+            img = Image.open(self._data_path / self.files[idx]).convert("RGB")
+            # convert to tensor
+            xfimg = self.xform(img)
+            return (xfimg, xfimg)
+
+    def __init__(self, args):
+        super().__init__(args)
+
+    @classmethod
+    def name(cls):
+        return "celeba_align"
+
+    @staticmethod
+    def add_arguments(parser):
+        # add rescale argument
+        parser.add_argument("-s", "--scale", type=int, help="rescale image size", default=256)
+
+    def get_fold(self, fold_name):
+        return self.DataFold(fold_name, scale=self.args()["scale"])
+
+
+def test(cmd_args):
+    print("Testing Celeba dataset:", cmd_args)
+    parser = argparse.ArgumentParser()
+    CelebaAlign.add_arguments(parser)
+    args = parser.parse_args(cmd_args)
+
+    celeba = CelebaAlign(vars(args))
+    print(f"Dataset name: {celeba.name()}")
+
+    # get fold
+    data = celeba.get_fold("train")
+
+    img, _ = data[0]
+    print(f"Dataset length: {len(data)}")
+    print(f"Image shape: {img.shape}")
+    print(f"Image dtype: {img.dtype}")
+    print(f"Image min: {img.min()}")
+
+    # index = data_path("cats_and_dogs") / "train.txt"
+    # with open(index, "r") as f:
+    #     files = f.read().splitlines()
+    # # open image with PIL
+    # for file in files:
+    #     print(file)
+    #     img = Image.open(data_path("cats_and_dogs") / file).convert("RGB")
+    #     img.load()
+
+
+# Test the Spiral dataset
+if __name__ == "__main__":
+    pass
---
+++ mlc/data/celeba_align/split.py
@@ -0,0 +1,64 @@
+import random
+
+from ...command.base import Base
+from ...util.resources import data_path
+
+
+class CelebAAlignSplit(Base):
+    def __init__(self, args):
+        super().__init__(args)
+
+    @classmethod
+    def name(cls):
+        return "celeba_align.split"
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument("-t", "--train", type=float, help="train split", default=0.7)
+        parser.add_argument("-v", "--val", type=float, help="validation split", default=0.2)
+        parser.add_argument("-T", "--test", type=float, help="test split", default=0.1)
+        parser.add_argument("--partial", type=float, help="partial split", default=1)
+
+    def run(self):
+        t = self.args.train
+        v = self.args.val
+        T = self.args.test
+        # normalize splits
+        s = t + v + T
+        t /= s
+        v /= s
+        T /= s
+        print(f"Splitting dataset into train:{t:0.02f}, validation:{v:0.02f} and test:{T:0.02f}")
+
+        files = list((data_path("celeba_align") / "data").glob("*.jpg"))
+
+        # better to use a seed for reproducibility
+        # or even numpy for better randomization
+        for i in range(5):
+            random.shuffle(files)
+
+        if self.args.partial < 1:
+            # partial split
+            n = int(len(files) * self.args.partial)
+            files = files[:n]
+
+        # make paths relative to dataset folder
+        files = [f.relative_to(data_path("celeba_align")) for f in files]
+
+        # split files
+        n = len(files)
+        n_train = int(n * t)
+        n_val = int(n * v)
+        train_files = files[:n_train]
+        val_files = files[n_train : n_train + n_val]
+        test_files = files[n_train + n_val :]
+
+        # save files
+        with open(data_path("celeba_align") / "train.txt", "w") as f:
+            f.write("\n".join([str(f) for f in train_files]))
+
+        with open(data_path("celeba_align") / "validation.txt", "w") as f:
+            f.write("\n".join([str(f) for f in val_files]))
+
+        with open(data_path("celeba_align") / "test.txt", "w") as f:
+            f.write("\n".join([str(f) for f in test_files]))
