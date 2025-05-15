651e94d1245a89d75603030e9ac8e7aa35d60743
João Pedro Del Rey <jpedrodelrey@gmail.com>
2025-05-14 21:35:44 -0300

    autoencoder finalizado

--- mlc/model/cnn_autoencoder/model.py
+++ mlc/model/cnn_autoencoder/model.py
@@ -4,8 +4,52 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torchsummary import summary
+from torchvision.models import VGG16_Weights, vgg16
+from torchvision.transforms.functional import resize
 
 from ..basemodel import BaseModel
+
+
+class VGGFeatureLoss(nn.Module):
+    def __init__(self, layer_indices=None):
+        super().__init__()
+
+        vgg = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).features
+        vgg.eval()
+
+        if layer_indices is None:
+            layer_indices = [3, 8, 15, 22, 29]
+        self.layer_indices = layer_indices
+
+        self.vgg_layers = nn.Sequential()
+        for i, layer in enumerate(vgg):
+            self.vgg_layers.add_module(str(i), layer)
+
+        for param in self.vgg_layers.parameters():
+            param.requires_grad = False
+        self.vgg_layers.eval()
+
+    def forward(self, input, target):
+        input = resize(input, (224, 224), antialias=True)
+        target = resize(target, (224, 224), antialias=True)
+
+        input_features = self.get_features(input)
+        target_features = self.get_features(target)
+
+        loss = 0
+        for i, (input_feature, target_feature) in enumerate(
+            zip(input_features, target_features)
+        ):
+            loss += nn.functional.mse_loss(input_feature, target_feature)
+        return loss
+
+    def get_features(self, x):
+        features = []
+        for i, layer in enumerate(self.vgg_layers):
+            x = layer(x)
+            if i in self.layer_indices:
+                features.append(x)
+        return features
 
 
 class ConvBlockDown(nn.Module):
@@ -27,6 +71,9 @@ class ConvBlockUp(nn.Module):
         super(ConvBlockUp, self).__init__()
         self.conv = nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+            nn.Conv2d(
+                out_channels, out_channels, kernel_size=2, stride=2
+            ),  # add to create interaction between pixels
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
@@ -38,44 +85,90 @@ class ConvBlockUp(nn.Module):
 class CNNAutoencoder(BaseModel):
     _name = "cnn_autoencoder"
 
-    def __init__(self, args):
+    def __init__(self, args: dict):
         super().__init__(args)
+        self.loss_type = args.get("loss_type", "BCE")
 
+        self.latent_sparsity = args.get("latent_sparsity", False)
+        if self.latent_sparsity:
+            assert args.get("sparsity_weight") is not None, (
+                "sparsity_weight must be set if latent_sparsity is True"
+            )
+            self.sparsity_weight = args.get("sparsity_weight", 0.1)
+        self.denoising = args.get("denoising", False)
+        if self.denoising:
+            assert args.get("noise_level") is not None, (
+                "noise_level must be set if denoising is True"
+            )
+            self.noise_level = args.get("noise_level", 0.1)
+        self.masking = args.get("masking", False)
+        self.last_mask = None
+        assert self.loss_type in ["VGG", "BCE", "L2"]
         self.epoch_counter = 0
+
+        self.loss_fn = None
+        self.last_z: torch.Tensor = None
+        match self.loss_type:
+            case "VGG":
+                self.loss_fn = lambda y_pred, y: 0.4 * VGGFeatureLoss()(
+                    y_pred, y
+                ) + 0.6 * F.mse_loss(y_pred, y)
+            case "BCE":
+                self.loss_fn = F.binary_cross_entropy_with_logits
+            case "L2":
+                self.loss_fn = F.mse_loss
 
         self.encoder = nn.Sequential(
             # down
-            ConvBlockDown(3, 64),  # 256x256x3 -> 128x128x64
-            ConvBlockDown(64, 128),  # 128x128x64 -> 64x64x128
-            ConvBlockDown(128, 256),  # 64x64x128 -> 32x32x256
-            ConvBlockDown(256, 512),  # 32x32x256 -> 16x16x512
-            ConvBlockDown(512, 1024),  # 16x16x512 -> 8x8x1024
-            ConvBlockDown(1024, 2048),  # 8x8x1024 -> 4x4x2048
+            ConvBlockDown(3, 128),  # 256x256x3 -> 128x128x128
+            ConvBlockDown(128, 512),  # 128x128x128 -> 64x64x512
+            ConvBlockDown(512, 1024),  # 64x64x512 -> 32x32x1024
+            ConvBlockDown(1024, 2048),  # 32x32x1024 -> 16x16x2048
         )
 
         self.decoder = nn.Sequential(
             ConvBlockUp(2048, 1024),  # 4x4x2048 -> 8x8x1024
             ConvBlockUp(1024, 512),  # 8x8x1024 -> 16x16x512
-            ConvBlockUp(512, 256),  # 16x16x512 -> 32x32x256
-            ConvBlockUp(256, 128),  # 32x32x256 -> 64x64x128
-            ConvBlockUp(128, 64),  # 64x64x128 -> 128x128x64
-            ConvBlockUp(64, 3),  # 128x128x64 -> 256x256x3
+            ConvBlockUp(512, 128),  # 16x16x512 -> 32x32x128
+            ConvBlockUp(128, 64),  # 32x32x128 -> 64x64x64
+            ConvBlockUp(64, 3),  # 64x64x64 -> 256x256x3
+            nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1),
         )
 
     @staticmethod
     def add_arguments(parser):
-        pass
+        parser.add_argument("--latent_sparsity", type=bool, default=False)
+        parser.add_argument("--loss_type", type=str, default="BCE")
+        parser.add_argument("--denoising", type=bool, default=False)
+        parser.add_argument("--masking", type=bool, default=False)
 
     def get_optimizer(self, learning_rate):
         return torch.optim.Adam(self.parameters(), lr=learning_rate)
-        # return torch.optim.SGD(self.parameters(), lr=learning_rate)
 
     def evaluate_loss(self, Y_pred, Y):
-        return torch.linalg.norm(Y_pred - Y, ord=2, dim=0).mean()
-        # return F.binary_cross_entropy(Y_pred, Y)
+        if self.masking:
+            # compute the loss only on the masked pixels
+            Y_pred = Y_pred * self.mask
+            Y = Y * self.mask
+        loss = self.loss_fn(Y_pred, Y)
 
-    def forward(self, x):
+        if self.latent_sparsity:
+            # compute l1 norm of the latent space to enforce sparsity
+            loss += self.sparsity_weight * F.l1_loss(
+                self.last_z, torch.zeros_like(self.last_z)
+            )
+        return loss
+
+    def forward(self, x: torch.Tensor):
+        if self.denoising:  # add noise to the input
+            noise = torch.randn_like(x, device=x.device) * self.noise_level
+            x = x + noise
+            x = torch.clamp(x, 0, 1)
+        if self.masking:
+            self.mask = torch.randint(0, 2, size=x.shape, device=x.device)
+            x = x * self.mask
         z = self.encoder(x)
+        self.last_z = z if self.training else None
         return self.decoder(z)
 
     def pre_epoch_hook(self, context):
@@ -98,7 +191,7 @@ class CNNAutoencoder(BaseModel):
         # get the model output
         with torch.no_grad():
             # move image to device
-            imgs = imgs.to("cuda")
+            imgs = imgs.to(device=context["device"])
             # get the model output
             if use_model:
                 imgs_out = self(imgs)
---
+++ mlc/model/cnn_vae/model.py
@@ -0,0 +1,261 @@
+import argparse
+import math
+
+import torch
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from torch import nn
+from torch.nn import functional as F
+from torchsummary import summary
+
+from ..basemodel import BaseModel
+
+
+class ConvBlockDown(nn.Module):
+    def __init__(self, in_channels, out_channels, batchnorm=True):
+        super(ConvBlockDown, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels) if batchnorm else nn.Identity(),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class ConvBlockUp(nn.Module):
+    def __init__(self, in_channels, out_channels, batchnorm=True):
+        super(ConvBlockUp, self).__init__()
+        self.conv = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+            nn.Conv2d(
+                out_channels, out_channels, kernel_size=2, stride=2
+            ),  # add to create interaction between pixels
+            nn.BatchNorm2d(out_channels) if batchnorm else nn.Identity(),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class ConvolutionalVAE(BaseModel):
+    def __init__(self, args: dict):
+        super().__init__(args)
+
+        init_dim = args["init_dim"]
+        layer_dim = init_dim
+        batch_norm = args.get("batchnorm", False)
+        self.z_dim = args["neck_dim"]
+        self.x_dim = 28 * 28
+        self.x_sigma = args["sigma"]
+
+        # used for logging
+        self._rec_loss = 0
+        self._kl_loss = 0
+
+        print(
+            f"ConvolutionalVAE: init_dim={init_dim}, layer_dim={layer_dim}, neck_dim={self.z_dim}"
+        )
+
+        self.encoder = nn.Sequential(
+            # down
+            ConvBlockDown(3, 128, batchnorm=batch_norm),  # 256x256x3 -> 128x128x128
+            ConvBlockDown(128, 512, batchnorm=batch_norm),  # 128x128x128 -> 64x64x512
+            ConvBlockDown(512, 1024, batchnorm=batch_norm),  # 64x64x512 -> 32x32x1024
+            ConvBlockDown(1024, 2048, batchnorm=batch_norm),  # 32x32x1024 -> 16x16x2048
+        )
+
+        self.decoder = nn.Sequential(
+            ConvBlockUp(2048, 1024, batchnorm=batch_norm),  # 4x4x2048 -> 8x8x1024
+            ConvBlockUp(1024, 512, batchnorm=batch_norm),  # 8x8x1024 -> 16x16x512
+            ConvBlockUp(512, 128, batchnorm=batch_norm),  # 16x16x512 -> 32x32x128
+            ConvBlockUp(128, 64, batchnorm=batch_norm),  # 32x32x128 -> 64x64x64
+            ConvBlockUp(64, 3, batchnorm=batch_norm),  # 64x64x64 -> 256x256x3
+        )
+
+    @classmethod
+    def name(cls):
+        return "conv_vae"
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument(
+            "--init-dim", type=int, default=128, help="First hidden layer dimension"
+        )
+        parser.add_argument("--neck-dim", type=int, default=16, help="Neck dimension")
+        parser.add_argument(
+            "--batchnorm", action="store_true", help="Use batch normalization"
+        )
+        parser.add_argument(
+            "--sigma",
+            type=float,
+            default=1,
+            help="\\sigma for P(x|z) = N(x|z, \\sigma)",
+        )
+        parser.set_defaults(batchnorm=False)
+        parser.add_argument("--log-zpca", action="store_true", help="Log z_\\mu PCA")
+        parser.set_defaults(log_zpca=False)
+
+    def get_optimizer(self, learning_rate, **kwargs):
+        return torch.optim.Adam(self.parameters(), lr=learning_rate)
+
+    def kl_divergence(self, z_mu, z_sigma2):
+        # Assuming sigma is a vector of the diagonal covariance matrix
+        tr_sigma = torch.sum(z_sigma2, dim=1)
+        muT_mu = (z_mu * z_mu).sum(dim=1)
+        # det_sigma = torch.prod(z_sigma2, dim=1) + 1e-10 * torch.ones_like(z_sigma2[:, 0])
+        log_det_sigma = torch.sum(torch.log(z_sigma2), dim=1)
+        # kl_loss = 0.5 * (tr_sigma + muT_mu - torch.log(det_sigma) - self.z_dim)
+        kl_loss = 0.5 * (tr_sigma + muT_mu - log_det_sigma - self.z_dim)
+        # print shapes
+        # print(f"tr_sigma: {tr_sigma.shape},
+        # muT_mu: {muT_mu.shape}, det_sigma: {det_sigma.shape}, kl_loss: {kl_loss.shape}")
+        # test if KL is negative
+        if torch.any(kl_loss < 0):
+            # get the index of the negative KL loss
+            neg_kl_indices = torch.where(kl_loss < 0)[0]
+            print("KL loss is negative")
+            print(f"kl_loss: {kl_loss[neg_kl_indices]}")
+            print("--")
+            print(f"trace: {tr_sigma[neg_kl_indices]}")
+            print(f"muT_mu: {muT_mu[neg_kl_indices]}")
+            # print(f"-log det: {-torch.log(det_sigma)[neg_kl_indices]}")
+            print(f"-log det: {-log_det_sigma[neg_kl_indices]}")
+            print(f"-z_dim: {-self.z_dim}")
+            print("--")
+            print(f"sigma: {z_sigma2[neg_kl_indices]}")
+            # print(f"det_sigma: {det_sigma[neg_kl_indices]}")
+            print(f"log_det_sigma_alt: {log_det_sigma[neg_kl_indices]}")
+            # print(f"det: {det_sigma[neg_kl_indices]}")
+            print(f"mu: {z_mu[neg_kl_indices]}")
+
+        return kl_loss
+
+    def reconstruction_loss(self, Y_pred, Y, x_sigma, x_dim):
+        s2_inv = 1.0 / (2.0 * x_sigma * x_sigma)
+        loss = -s2_inv * F.mse_loss(
+            Y_pred, Y.flatten(start_dim=1), reduction="none"
+        ).sum(dim=1)
+        # loss += -0.5 * x_dim * math.log(2 * x_sigma * x_sigma * math.pi) * torch.ones_like(loss)
+        # print(f"> {-0.5 * x_dim * math.log(2 * x_sigma * x_sigma * math.pi)}")
+        return loss
+
+    def evaluate_loss(self, Y_pred, Y):
+        rec_loss = self.reconstruction_loss(Y_pred, Y, self.x_sigma, self.x_dim).mean()
+        self._rec_loss += -rec_loss.item() * len(Y)
+        kl_loss = self.kl_divergence(self._z_mu, self._z_sigma2).mean()
+        self._kl_loss += kl_loss.item() * len(Y)
+        return -1.0 * (rec_loss - kl_loss)
+
+    def forward(self, x):
+        # q_mu_logsigma has the form (mu, logsigma)
+        q_mu_logsigma2 = self.encoder(x)
+        self._z_mu = q_mu_logsigma2[:, : self.z_dim]
+        self._z_sigma2 = torch.exp(q_mu_logsigma2[:, self.z_dim :])
+        z_sigma = torch.sqrt(self._z_sigma2)
+        # reparameterization trick
+        eps = torch.randn_like(self._z_mu)
+        # print(f"mu: {mu.shape}, sigma: {sigma.shape}, eps: {eps.shape}")
+        z = self._z_mu + eps * z_sigma
+        return self.decoder(z)
+
+    def _reset_losses(self):
+        self._rec_loss = 0
+        self._kl_loss = 0
+
+    def _log_images(self, context, epoch=None, use_model=True):
+        # log some images to
+        validation_data_loader = context["validation_data_loader"]
+        # get a batch of 8 random images
+        images = next(iter(validation_data_loader))
+        imgs = images[0][0:8]
+        # get the model output
+        with torch.no_grad():
+            # move image to device
+            imgs = imgs.to(context["device"])
+            # get the model output
+            if use_model:
+                imgs_out = self(imgs)
+            else:
+                imgs_out = imgs
+            # save the image
+        for i in range(8):
+            img = imgs_out[i].view(1, 28, 28)
+            context["board"].log_image(f"Images/Image_{i}", img, epoch)
+
+    def _log_losses(self, context, data_loader, name):
+        board = context["board"]
+        rec_loss = self._rec_loss / len(data_loader.dataset)
+        kl_loss = self._kl_loss / len(data_loader.dataset)
+        board.log_scalars(
+            "Curves/Loss",
+            {f"Reconstruction_{name}": rec_loss, f"KLDivergence_{name}": kl_loss},
+            context["epoch"],
+        )
+
+    def pre_epoch_hook(self, context):
+        epoch = context["epoch"]
+        if epoch == 1:
+            self._log_images(context, 0, False)
+
+    def pre_validation_hook(self, context):
+        self._reset_losses()
+        if self.args["log_zpca"]:
+            self._z_mu_list = list()
+
+    def post_validation_batch_hook(self, context, X, Y, Y_pred, loss):
+        # log the z_mu
+        if self.args["log_zpca"]:
+            self._z_mu_list.append(self._z_mu)
+
+    def pre_train_hook(self, context):
+        self._reset_losses()
+
+    def post_train_hook(self, context):
+        self._log_losses(context, context["train_data_loader"], "Train")
+
+    def post_validation_hook(self, context):
+        self._log_losses(context, context["validation_data_loader"], "Validation")
+        if context["epoch"] >= 0:
+            self._log_images(context, context["epoch"], True)
+            if self.args["log_zpca"]:
+                z_mu = torch.cat(self._z_mu_list, dim=0)
+                # compute PCA using sklearn
+                pca = PCA()  # get all components
+                z_pca = pca.fit_transform(z_mu.cpu().numpy())
+                fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+                ax[0].scatter(z_pca[:, 0], z_pca[:, 1], s=1)
+                ax[0].set_title("PCA of z_mu")
+                ax[0].set_xlabel("PCA 1")
+                ax[0].set_ylabel("PCA 2")
+                ax[0].set_xlim(-3, 3)
+                ax[0].set_ylim(-3, 3)
+                ax[0].grid()
+                # plot the explained variance
+                ax[1].bar(
+                    range(1, len(pca.explained_variance_ratio_) + 1),
+                    pca.explained_variance_ratio_,
+                )
+                ax[1].set_title("Explained variance")
+                context["board"].log_figure("PCA/z_mu", fig, context["epoch"])
+                plt.close(fig)
+
+
+def test(args):
+    print("Testing CNNVariationalAutoencoder model:", args)
+
+    parser = argparse.ArgumentParser()
+    ConvolutionalVAE.add_arguments(parser)
+    args = parser.parse_args(args)
+
+    model = ConvolutionalVAE(vars(args))
+    print(f"Model name: {model.name()}")
+    summary(model.encoder, (28, 28), device="cpu")
+    summary(model.decoder, (16,), device="cpu")
+
+
+if __name__ == "__main__":
+    pass
