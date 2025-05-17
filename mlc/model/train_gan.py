Enhance MNIST dataset handling; add noise and normalization options, ensure correct input shape, and update DataFold initialization. Refactor MLPGAN model to adjust layer biases and improve generator and discriminator optimizer settings.

mlc/model/train_gan.py
@@ -43,7 +43,7 @@ class TrainGAN(Base):
         parser.add_argument("-s", "--seed", type=int, default=42)  # TODO: use seed
         parser.add_argument("-e", "--epochs", type=int, required=True)
         parser.add_argument("-d", "--device", type=_parse_device_arg, default="cuda", help="Device to use for training")
-        parser.add_argument("-l", "--learning-rate", type=float, default=0.0001)
+        parser.add_argument("-l", "--learning-rate", type=float, default=0.00001)
         parser.add_argument("-b", "--batch-size", type=int, default=32)
         parser.add_argument("-c", "--check-point", type=int, default=10, help="Check point every n epochs")
         parser.add_argument("-t", "--tensorboard", action="store_true", help="Enable tensorboard logging")
@@ -145,8 +145,8 @@ class TrainGAN(Base):
                 torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
                 torch.nn.init.constant_(m.bias.data, 0)
 
-        model.discriminator.apply(weights_init)
-        model.generator.apply(weights_init)
+        # model.discriminator.apply(weights_init)
+        # model.generator.apply(weights_init)
 
         try:  # let's catch keyboard interrupt
             pbar = tqdm(range(1 + delta_e, self.args["epochs"] + 1 + delta_e))
@@ -197,7 +197,9 @@ class TrainGAN(Base):
 
                     # now compute loss on fake data
                     Z = torch.randn(X_train.size(0), model.latent_dimension(), device=self.device)
-                    X_fake = model.generator(Z)
+                    with torch.no_grad():
+                        # generate fake data
+                        X_fake = model.generator(Z)
                     Y_label.fill_(0.0)
                     Y_pred = torch.sigmoid(model.discriminator(X_fake.detach()))
                     d_train_loss_fake = model.evaluate_discriminator_loss(
@@ -217,10 +219,13 @@ class TrainGAN(Base):
                     generator_optimizer.zero_grad()
 
                     # lets use the allready generated data
+                    Z = torch.randn(X_train.size(0), model.latent_dimension(), device=self.device)
+                    X_fake = model.generator(Z)
                     Y_pred = torch.sigmoid(model.discriminator(X_fake))
                     g_train_loss = model.evaluate_generator_loss(Y_pred)
                     g_train_loss.backward()
                     DG_z2 += torch.sum(Y_pred).item()
+                    # if epoch > 40:
                     generator_optimizer.step()
 
                     # call post_batch_hook