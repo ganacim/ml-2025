b0ad219de41c300b86f267019ade449036d77697
Francisco Ganacim <ganacim@impa.br>
2025-05-21 13:52:19 -0300

    Adjust weight initialization for discriminator in TrainGAN; modify standard deviation to improve model stability.

--- mlc/model/train_gan.py
+++ mlc/model/train_gan.py
@@ -141,9 +141,9 @@ class TrainGAN(Base):
         def weights_init(m):
             classname = m.__class__.__name__
             if classname.find("Linear") != -1:
-                torch.nn.init.normal_(m.weight.data, 0.0, 0.2)
+                torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
             elif classname.find("BatchNorm") != -1:
-                torch.nn.init.normal_(m.weight.data, 1.0, 0.2)
+                torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
                 torch.nn.init.constant_(m.bias.data, 0)
 
         model.discriminator.apply(weights_init)
