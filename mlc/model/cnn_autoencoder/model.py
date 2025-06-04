0f775027ab9b38d2a221ad0bb7bf4d921a1f15b2
Humberto Seghetto <hseghetto@gmail.com>
2025-06-04 01:11:22 -0300

    Linear Z-Dim tests

--- mlc/model/cnn_autoencoder/model.py
+++ mlc/model/cnn_autoencoder/model.py
@@ -24,13 +24,15 @@ class CNNAutoencoder(BaseModel):
         else:
             raise ValueError(f"Unknown loss function: {args['loss_function']}")
 
-        init_dim = args["init_dim"]
-        layer_dim = init_dim
-        num_blocks = args["num_blocks"]
-        image_channels = args["image_channels"]
-        kernel_size = 3
 
-        print(f"CNNAutoencoder: init_dim={init_dim}, layer_dim={layer_dim}, num_blocks={num_blocks}")
+        self.init_dim = args["init_dim"]
+        self.layer_dim = self.init_dim
+        self.num_blocks = args["num_blocks"]
+        self.image_channels = args["image_channels"]
+        self.image_size = args.get("image_size", 64)
+        self.kernel_size = 3
+
+        print(f"CNNAutoencoder: init_dim={self.init_dim}, layer_dim={self.layer_dim}, num_blocks={self.num_blocks}")
 
         Normalization = nn.BatchNorm2d if args["batchnorm"] else nn.Identity
         bias = False if args["batchnorm"] else True
@@ -39,55 +41,63 @@ class CNNAutoencoder(BaseModel):
         use_maxpool = args.get("maxpool", False)
         
         enc_layers = []
-        for i in range(num_blocks):
+        for i in range(self.num_blocks):
             if use_maxpool:
                 enc_layers += [
                     nn.MaxPool2d(2, stride = 2, padding = 0),
-                    nn.Conv2d(layer_dim, layer_dim, kernel_size, bias=bias, padding= kernel_size//2),
-                    nn.Conv2d(layer_dim, layer_dim, kernel_size, bias=bias, padding= kernel_size//2),
+                    nn.Conv2d(self.layer_dim, self.layer_dim, self.kernel_size, bias=bias, padding= self.kernel_size//2),
+                    nn.Conv2d(self.layer_dim, self.layer_dim, self.kernel_size, bias=bias, padding= self.kernel_size//2),
                     nn.ReLU(inplace=True),
-                    Normalization(layer_dim),
+                    Normalization(self.layer_dim),
                 ]
             else:
                 enc_layers += [
-                    nn.Conv2d(layer_dim, layer_dim, kernel_size, stride= 2, bias=bias, padding= kernel_size//2),
-                    nn.Conv2d(layer_dim, layer_dim, kernel_size, bias=bias, padding= kernel_size//2),
+                    nn.Conv2d(self.layer_dim, self.layer_dim, self.kernel_size, stride= 2, bias=bias, padding= self.kernel_size//2),
+                    nn.Conv2d(self.layer_dim, self.layer_dim, self.kernel_size, bias=bias, padding= self.kernel_size//2),
                     nn.ReLU(inplace=True),
-                    Normalization(layer_dim),
+                    Normalization(self.layer_dim),
                 ]
             #layer_dim = layer_dim * 2
 
         self.encoder = nn.Sequential(
             # down
-            nn.Conv2d(image_channels, init_dim, kernel_size, bias=bias, padding= kernel_size//2),
+            nn.Conv2d(self.image_channels, self.init_dim, self.kernel_size, bias=bias, padding= self.kernel_size//2),
             nn.ReLU(inplace=True),
-            Normalization(init_dim),
+            Normalization(self.init_dim),
             *enc_layers,
         )
- 
+
+        self.z_dim = args["z_dim"]
+        self.layer_mult = (self.image_size // (2 ** self.num_blocks))
+        
+        self.z_encode = nn.Linear(self.layer_dim * self.layer_mult ** 2, self.z_dim, bias=bias)
+        self.z_decode = nn.Linear(self.z_dim, self.layer_dim * self.layer_mult ** 2, bias=bias)
+
         dec_layers = []
-        for i in range(num_blocks):
+        for i in range(self.num_blocks):
             dec_layers += [
                 #nn.Upsample(scale_factor = 2),
                 #nn.Conv2d(layer_dim, layer_dim, kernel_size, bias=bias, padding= kernel_size//2),
                 #nn.Conv2d(layer_dim, layer_dim, kernel_size, bias=bias, padding= kernel_size//2),
-                nn.ConvTranspose2d(layer_dim, layer_dim, kernel_size=3, stride=2, bias=bias, padding=1, output_padding=1),
-                nn.Conv2d(layer_dim, layer_dim, kernel_size, bias=bias, padding= kernel_size//2),
+                nn.ConvTranspose2d(self.layer_dim, self.layer_dim, kernel_size=3, stride=2, bias=bias, padding=1, output_padding=1),
+                nn.Conv2d(self.layer_dim, self.layer_dim, self.kernel_size, bias=bias, padding= self.kernel_size//2),
                 nn.ReLU(inplace=True),
-                Normalization(layer_dim),
+                Normalization(self.layer_dim),
             ]
             #layer_dim = layer_dim // 2
         self.decoder = nn.Sequential(
             *dec_layers,
-            nn.Conv2d(layer_dim, init_dim, 3, padding="same"),
+            nn.Conv2d(self.layer_dim, self.init_dim, 3, padding="same"),
             nn.ReLU(inplace=True),
-            nn.Conv2d(layer_dim, image_channels, 1, padding="same"),
+            nn.Conv2d(self.layer_dim, self.image_channels, 1, padding="same"),
             sigmoid,
         )
 
     @staticmethod
     def add_arguments(parser):
         parser.add_argument("--init-dim", type=int, default=24, help="First Conv2d number of channels")
+        parser.add_argument("--image-dim", type=int, default=64, help="Image size (height and width)")
+        parser.add_argument("--z-dim", type=int, default=128, help="Size of the latent space (z)")
         parser.add_argument("--num-blocks", type=int, default=4, help="Number of Encoding blocks")
         parser.add_argument("--batchnorm", action="store_true", help="Use batch normalization")
         parser.add_argument("--sigmoid", action="store_true", help="Use sigmoid activation in the last layer")
@@ -107,6 +117,10 @@ class CNNAutoencoder(BaseModel):
 
     def forward(self, x):
         z = self.encoder(x)
+        z = torch.flatten(z, start_dim=1)  # flatten the output for encoding
+        z = self.z_encode(z)
+        z = self.z_decode(z)
+        z = z.view(z.size(0), self.layer_dim, self.layer_mult, self.layer_mult)
         return self.decoder(z)
 
     def pre_epoch_hook(self, context):
