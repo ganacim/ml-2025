ffb16962df29b869c607daa38efcc31046df3fee
kauan <kauancdmoraes1@gmail.com>
2025-06-06 23:28:45 -0300

    MODIFY model_gan

--- mlc/model/gan_vae/model_gan.py
+++ mlc/model/gan_vae/model_gan.py
@@ -14,7 +14,7 @@ class GANVAE(BaseModel):
         super().__init__(args)
 
         self.epoch = 0
-        self.x_dim = 28 * 28
+        self.x_dim = 256 * 256
 
         gen_dims = args["generator_dims"]
         dis_dims = args["discriminator_dims"]
@@ -30,56 +30,55 @@ class GANVAE(BaseModel):
         # will send to device later
         self.z_samples = torch.randn(16, self.z_dim)
 
-        Normalization = nn.Identity
-        bias = True
-        if use_batchnorm == "generator" or use_batchnorm == "both":
-            Normalization = nn.BatchNorm1d
-            bias = False
 
-        print(f"MLPGAN: generator_dims={gen_dims}, discriminator_dims={dis_dims}")
+        print(f"GANVAE: generator_dims={gen_dims}, discriminator_dims={dis_dims}")
+
+        # generator
 
         g = nn.LeakyReLU(leakyness)  # ReLu() #PReLU() #nn.Sigmoid()
         gen_layers = []
-        for i in range(1, len(hidden_chs)):
+        for i in range(len(hidden_chs) - 1, 1, -1):
             gen_layers += [
-                nn.Conv2d(hidden_chs[i - 1], hidden_chs[i], kernel_size=3, stride=1, padding=1),
+                        nn.ConvTranspose2d(hidden_chs[i], hidden_chs[i - 1], kernel_size=2, stride=2, padding=0),
+                        nn.Conv2d(hidden_chs[i - 1], hidden_chs[i - 1], kernel_size=3, stride=1, padding=1),
+                        nn.BatchNorm2d(hidden_chs[i - 1]),
+                        nn.LeakyReLU(leakyness),
+                    ]
+
+        self.generator = nn.Sequential(
+            *gen_layers,
+            nn.Conv2d(hidden_chs[-1], hidden_chs[-1], kernel_size=3, stride=1, padding=1),
+            nn.Tanh(),
+        )
+  
+        
+        # discriminator
+        dis_layers = []
+        g = nn.LeakyReLU(leakyness)  # PReLU() #nn.Sigmoid()
+        for i in range(1, len(hidden_chs)):
+            # if i%2==0:
+            #     g = nn.ReLU()
+            # else:
+            dis_layers += [
+                nn.Conv2d(hidden_chs[i - 1], hidden_chs[i], kernel_size=3, stride=2, padding=1),
                 nn.BatchNorm2d(hidden_chs[i]),
                 g,
                 nn.Conv2d(hidden_chs[i], hidden_chs[i], kernel_size=3, stride=1, padding=1),
                 nn.BatchNorm2d(hidden_chs[i]),
                 g,
-                nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             ]
+        # VAE needs to output mu and logsigma
 
-        self.generator = nn.Sequential(
-            *gen_layers,
-            nn.Conv2d(gen_dims[-1], self.x_dim, bias=False),
-            nn.Tanh(),
-        )
-
-        Normalization = nn.Identity
-        bias = True
-        if use_batchnorm == "discriminator" or use_batchnorm == "both":
-            Normalization = nn.BatchNorm1d
-            bias = False
-
-        dis_layers = []
-        for i in range(len(dis_dims) - 1):
-            dis_layers += [
-                nn.Linear(dis_dims[i], dis_dims[i + 1], bias=bias),
-                Normalization(dis_dims[i + 1]),
-                nn.LeakyReLU(leakyness),
-            ]
-
+        
+        # deixar 2x2 com 16 canais
         self.discriminator = nn.Sequential(
             # down
             nn.Dropout(args["discriminator_dropout"]),
-            nn.Linear(self.x_dim, dis_dims[0], bias=True),
-            # do not use batchnorm on the first layer
+            nn.Conv2d(hidden_chs[0], hidden_chs[0], kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(leakyness),
             *dis_layers,
-            # classifier
-            nn.Linear(dis_dims[-1], 1, bias=True),
+            nn.Conv2d(hidden_chs[-1], hidden_chs[-1], kernel_size=3, stride=1, padding=1),
+            nn.Flatten(),
         )
 
     @staticmethod
@@ -88,7 +87,7 @@ class GANVAE(BaseModel):
         parser.add_argument("--generator-dims", type=int, nargs="+", default=[128, 256])
         # discriminator dimensions is a list
         parser.add_argument("--discriminator-dims", type=int, nargs="+", default=[256, 128])
-        parser.add_argument("--discriminator-dropout", type=float, default=0)
+        parser.add_argument("--discriminator-dropout", type=float, default=0.1)
         parser.add_argument("--batchnorm", choices=["generator", "discriminator", "both", "none"], default="none")
         parser.add_argument("--leakyness", type=float, default=0.01, help="LeakyReLU leakyness")
         parser.add_argument("--init", choices=["both", "none", "discriminator", "generator"], default="generator")
@@ -165,10 +164,10 @@ def test(args):
     print("Testing GANVAE model:", args)
 
     parser = argparse.ArgumentParser()
-    MLPGAN.add_arguments(parser)
+    GANVAE.add_arguments(parser)
     args = parser.parse_args(args)
 
-    model = MLPGAN(vars(args))
+    model = GANVAE(vars(args))
     print(f"Model name: {model.name()}")
     summary(model.generator, (model.latent_dimension(),), device="cpu")
     summary(model.discriminator, (28 * 28,), device="cpu")
