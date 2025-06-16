21cfea4c9553d9fe711d9164190f880dc67b3bbd
Melvin Poveda <melvinpqbsc@gmail.com>
2025-06-16 20:15:00 +0000

    Add reward

--- mlc/reinforcement/train.py
+++ mlc/reinforcement/train.py
@@ -1,22 +1,18 @@
 import argparse
-import gymnasium as gym
-from gymnasium.wrappers import RecordVideo
-from time import sleep
 import re
-import random
-import torch
-from torch import nn
-from mlc.reinforcement.networks import MLP
-import numpy as np
-from tqdm import tqdm
-import ale_py
-from torch.utils.tensorboard.writer import SummaryWriter
-
 from collections import deque
 
+import ale_py
+import gymnasium as gym
+import numpy as np
+import torch
+from torch.utils.tensorboard.writer import SummaryWriter
+from tqdm import tqdm
 
 from mlc.command.base import Base
+from mlc.reinforcement.networks import MLP
 from mlc.util.resources import get_time_as_str
+
 
 class Train(Base):
 
@@ -55,7 +51,7 @@ class Train(Base):
         parser.add_argument("--num_envs", default=4, type=int)
         parser.add_argument("-d", "--device", type=_parse_device_arg, default="cuda", help="device to use for training")
         parser.add_argument("-l", "--learning-rate", type=float, default=0.001)
-        #parser.add_argument("-b", "--batch-size", type=int, default=32)
+        # parser.add_argument("-b", "--batch-size", type=int, default=32)
         parser.add_argument("-c", "--check-point", type=int, default=100, help="check point every n episodes")
         parser.add_argument("-v", "--video", type=int, default=100, help="create a video every n episodes")
         parser.add_argument("-p", "--personal", action="store_true", help="enable personal folder")
@@ -66,14 +62,13 @@ class Train(Base):
         game = self.hparams["game"]
         torch.autograd.set_detect_anomaly(True)
         num_envs = self.hparams["num_envs"]
-        #envs = gym.make_vec(game, render_mode=None, vectorization_mode="async", num_envs=num_envs)
+        # envs = gym.make_vec(game, render_mode=None, vectorization_mode="async", num_envs=num_envs)
 
         envs = gym.vector.AsyncVectorEnv(
-            [lambda: gym.make(game) for _ in range(num_envs)],
-            autoreset_mode=gym.vector.AutoresetMode.NEXT_STEP
+            [lambda: gym.make(game) for _ in range(num_envs)], autoreset_mode=gym.vector.AutoresetMode.NEXT_STEP
         )
 
-        device = 'cpu'
+        device = "cpu"
 
         s, _ = envs.reset()
         s = torch.tensor(s, dtype=torch.float32).flatten(start_dim=1).to(device)
@@ -81,15 +76,13 @@ class Train(Base):
         n_actions = int(envs.action_space[0].n)
         all_actions = list(range(n_actions))
         policy_nn = MLP(
-            dim_input = s.shape[-1],
-            dim_output = n_actions,
+            dim_input=s.shape[-1],
+            dim_output=n_actions,
         ).to(device)
-
 
         learning_rate = torch.tensor(self.hparams["learning_rate"], dtype=torch.float32).to(device)
         optimizer = torch.optim.Adam(policy_nn.parameters(), lr=learning_rate)
         pbar = tqdm()
-
 
         states, info = envs.reset()
         states_new = torch.tensor(states, dtype=torch.float32).flatten(start_dim=1).to(device) / 255
@@ -100,10 +93,9 @@ class Train(Base):
         for i in range(envs.num_envs):
             replay_buffers.append(deque(maxlen=4096))
 
-
         episode_start = np.zeros(envs.num_envs, dtype=bool)
 
-        max_reward = -9999
+        # max_reward = -9999
         n_episodes = 0
         while True:
 
@@ -118,21 +110,22 @@ class Train(Base):
             # vectorized step
             aux_states, rewards, terminations, truncations, info = envs.step(actions)
 
-
             states_old = states_new
             states_new = torch.tensor(aux_states, dtype=torch.float32).flatten(start_dim=1).to(device) / 255
 
             for i in range(envs.num_envs):
 
                 if not episode_start[i]:
-                    replay_buffers[i].append({
-                        "state":states[i],
-                        "frame": aux_states[i],
-                        "action": int(actions[i]),
-                        "reward": float(rewards[i]),
-                        "termination": bool(terminations[i]),
-                        "truncation": bool(truncations[i]),
-                    })
+                    replay_buffers[i].append(
+                        {
+                            "state": states[i],
+                            "frame": aux_states[i],
+                            "action": int(actions[i]),
+                            "reward": float(rewards[i]),
+                            "termination": bool(terminations[i]),
+                            "truncation": bool(truncations[i]),
+                        }
+                    )
 
             states = states_new - states_old
             episode_start = np.logical_or(terminations, truncations)
@@ -143,51 +136,52 @@ class Train(Base):
                     pbar.update()
                     replay = list(replay_buffers[i])
 
-
                     # find start of last termination
                     j0 = -1
                     for j, r in enumerate(replay[:-1]):
-                        if r['termination'] or r['truncation']:
+                        if r["termination"] or r["truncation"]:
                             j0 = j
-                    j0+=1
+                    j0 += 1
                     replay = replay[j0:]
 
                     rewards = [x["reward"] for x in replay]
+                    actions_game = [x["action"] for x in replay]
                     sum_rewards = sum(rewards)
-                    self.writer.add_scalar('reward', sum_rewards, n_episodes)
+                    self.writer.add_scalar("reward", sum_rewards, n_episodes)
 
                     propagated_rewards = []
 
                     running_mean = 0
-                    for R in rewards[::-1]:
-                        if abs(R) > .5:
-                            running_mean = 0
-                        running_mean = R + .99 * running_mean
+                    for R, a in zip(rewards[::-1], actions_game[::-1]):
+                        if a == 1 or a == 7 or a == 8:
+                            R += 0.5
+                        # if abs(R) > .5:
+                        #     running_mean = 0
+                        running_mean = R + 0.99 * running_mean
                         propagated_rewards.insert(0, running_mean)
 
-                    n_nonzero_rewards = sum([abs(x)>.5 for x in rewards])
+                    n_nonzero_rewards = sum([abs(x) > 0.5 for x in rewards])
                     replay_states = torch.stack([x["state"] for x in replay])
 
-
-
-                    if n_nonzero_rewards >= 0:
+                    if n_nonzero_rewards > 0:
                         preds = policy_nn(replay_states)
                         loss = torch.tensor(0, dtype=torch.float32).to(device)
                         for j, r in enumerate(replay):
-                            loss += -torch.log(10e-9 + preds[j][r["action"]])*propagated_rewards[j]/n_nonzero_rewards
+                            loss += (
+                                -torch.log(10e-9 + preds[j][r["action"]]) * propagated_rewards[j] / n_nonzero_rewards
+                            )
 
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
 
-                    if (n_episodes-1) % self.hparams["video"] == 0:
+                    if (n_episodes - 1) % self.hparams["video"] == 0:
                         frames = np.stack([x["frame"] for x in replay])
-                        frames = np.permute_dims(frames, (0,3,1,2))
+                        frames = np.permute_dims(frames, (0, 3, 1, 2))
                         frames = np.expand_dims(frames, axis=0)
-                        self.writer.add_video('gameplay', frames, n_episodes, fps=30)
+                        self.writer.add_video("gameplay", frames, n_episodes, fps=30)
 
                     self.writer.flush()
-
 
 
 if __name__ == "__main__":
