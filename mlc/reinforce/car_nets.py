2db936c9b40e5edfabe14b49fba11d551e41a12d
kauan <kauancdmoraes1@gmail.com>
2025-06-19 06:06:44 -0300

    rodando modo continuo

--- mlc/reinforce/car_nets.py
+++ mlc/reinforce/car_nets.py
@@ -2,11 +2,12 @@ from torch import nn
 
 
 class Modelo(nn.Module):
-    def __init__(self, modo = "discrete", dim_input=4, dim_outdis=5, dim_out_cont= 3, dim_hidden=64, init_ch=3):
+    def __init__(self, mode = "discrete", dim_input=4, dim_outdis=5, dim_out_cont= 3, dim_hidden=64, init_ch=3):
         super().__init__()
-        if modo == "discrete":
+        self.modo = mode
+        if self.modo == "discrete":
             self.dim_out = dim_outdis
-        elif modo == "continuous":
+        elif self.modo == "continuous":
             self.dim_out = dim_out_cont
         hidden_chs = [init_ch] + [8, 16, 32, 64, 128, 256]
 
--- mlc/reinforce/train_car.py
+++ mlc/reinforce/train_car.py
@@ -32,7 +32,10 @@ class TrainCar(Base):
         self.hparams = hparams
         self.gamma = hparams["gamma"]
         self.mode = hparams["mode"]
-        self.output_folder = f"agents/{hparams['game'].replace('/', '_')}/car_agent/{get_time_as_str()}"
+        if hparams["name"]:
+            self.output_folder = f"agents/{hparams['game'].replace('/', '_')}/car_agent/{hparams['name']}"
+        else:
+            self.output_folder = f"agents/{hparams['game'].replace('/', '_')}/car_agent/{get_time_as_str()}"
         self.writer = SummaryWriter(self.output_folder + "/tensorboard")
         gym.register_envs(ale_py)
 
@@ -52,7 +55,7 @@ class TrainCar(Base):
         parser.add_argument("-e", "--max_episodes", type=int, default=100000)
 
         parser.add_argument("-g", "--game", default="CarRacing-v3")
-        parser.add_argument("--num_envs", default=8, type=int)
+        parser.add_argument("--num_envs", default=4, type=int)
         parser.add_argument("-d", "--device", type=_parse_device_arg, default="cuda", help="device to use for training")
         parser.add_argument("-l", "--learning-rate", type=float, default=0.008, help="learning rate for the optimizer")
         # parser.add_argument("-b", "--batch-size", type=int, default=32)
@@ -82,24 +85,24 @@ class TrainCar(Base):
         )
 
         device = self.device
-        envs = envs.to(device)
 
         s, _ = envs.reset(options={"randomize": False})
         s = torch.tensor(s, dtype=torch.float32).to(device) #.flatten(start_dim=1)
 
         if self.mode == "discrete":
-            n_actions = int(envs.action_space[0].n)
+            n_actions = envs.single_action_space.n
             all_actions = list(range(n_actions))
         else:
-            n_actions = envs.action_space[0].shape[0]
-            all_actions = np.arange(n_actions)
+            n_actions = envs.single_action_space.shape[0]
+            action_space_low = torch.tensor(envs.single_action_space.low, device=device, dtype=torch.float32)
+            action_space_high = torch.tensor(envs.single_action_space.high, device=device, dtype=torch.float32)
 
-        policy_nn = Modelo(mode = self.mode, init_ch=3,
+        policy_nn = Modelo(mode=self.mode, init_ch=3,
                         dim_hidden=64,
                         dim_input=s.shape[-1],
                     ).to(device)
 
-        learning_rate = torch.tensor(self.hparams["learning_rate"], dtype=torch.float32).to(device)
+        learning_rate = torch.tensor(self.hparams["learning_rate"], dtype=torch.float32)
         optimizer = torch.optim.Adam(policy_nn.parameters(), lr=learning_rate)
         pbar = tqdm()
 
@@ -124,16 +127,20 @@ class TrainCar(Base):
 
             # sample actions
             actions = []
-            for i in range(num_envs):
-                if self.mode == "continuous":
-                    st,gas,br = policy_nn(states[i])
-                    steering = np.clip(nn.Tanh()(st).cpu().numpy(), -1, 1)
-                    gas = np.clip(nn.Sigmoid()(gas).cpu().numpy(), 0, 1)
-                    br = np.clip(nn.Sigmoid()(br).cpu().numpy(), 0, 1)
-                    actions.append(np.array([st, gas, br], dtype=np.float32))
-                    std = torch.tensor([0.1, 0.1, 0.1], dtype=torch.float32).to(device)
-                    actions[i] += torch.normal(mean=actions[i], std=std)
-                else:
+            if self.mode == "continuous":
+                action_logits = policy_nn(states)
+                steering = torch.tanh(action_logits[:,0])
+                gas = torch.sigmoid(action_logits[:,1])
+                br = torch.sigmoid(action_logits[:,2])
+                actions_mu = torch.stack([steering, gas, br], dim=1)
+                
+                std = torch.tensor([0.1, 0.1, 0.1], dtype=torch.float32).to(device)
+                action_dist = torch.distributions.Normal(actions_mu, std)
+                actions_tensor = action_dist.sample()
+                actions_tensor = torch.max(torch.min(actions_tensor, action_space_high), action_space_low)
+                actions = actions_tensor.cpu().numpy()
+            else:
+                for i in range(num_envs):               
                     actions.append(np.random.choice(all_actions, p=action_dist[i]))
 
             # vectorized step
@@ -149,12 +156,16 @@ class TrainCar(Base):
                         {
                             "state": states[i],
                             "frame": aux_states[i],
-                            "action": int(actions[i]),
+                            
                             "reward": float(rewards[i]),
                             "termination": bool(terminations[i]),
                             "truncation": bool(truncations[i]),
                         }
                     )
+                    if self.mode == "continuous":
+                        replay_buffers[i][-1]["action"] = actions_tensor[i].cpu().numpy()
+                    else:
+                        replay_buffers[i][-1]["action"] = int(actions[i])
 
             states = states_new - states_old
             episode_start = np.logical_or(terminations, truncations)
@@ -192,8 +203,20 @@ class TrainCar(Base):
 
                     n_nonzero_rewards = sum([abs(x) > 0.5 for x in rewards])
                     replay_states = torch.stack([x["state"] for x in replay]).to(device)
-                    
-                    if n_nonzero_rewards > 0:  # Changed from >= to >
+                    if self.mode == "continuous":
+                        replay_actions = torch.tensor(np.array([x["action"] for x in replay]), dtype=torch.float32, device=device)
+                        action_logits = policy_nn(replay_states)
+                        mu_steer = torch.tanh(action_logits[:, 0])
+                        mu_gas = torch.sigmoid(action_logits[:, 1])
+                        mu_brake = torch.sigmoid(action_logits[:, 2])
+                        action_mu = torch.stack([mu_steer, mu_gas, mu_brake], dim=1)
+                        action_std = torch.tensor([0.1, 0.1, 0.1], device=device)
+                        action_dist = torch.distributions.Normal(action_mu, action_std)
+                        log_probs = action_dist.log_prob(replay_actions).sum(dim=1)
+                        #propagated_rewards = (propagated_rewards - propagated_rewards.mean()) / (propagated_rewards.std() + 1e-9)
+                        loss = torch.dot(-log_probs, torch.tensor(np.array(propagated_rewards), dtype=torch.float32).to(device)) 
+                        loss = torch.tensor(loss, dtype=torch.float32).to(device)        
+                    elif n_nonzero_rewards > 0:  # Changed from >= to >
                         preds = policy_nn(replay_states)
                         loss = torch.tensor(0, dtype=torch.float32).to(device)
                         for j, r in enumerate(replay):
@@ -204,6 +227,9 @@ class TrainCar(Base):
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
+
+
+                    self.writer.add_scalar("loss", loss.item(), n_episodes)
 
                     if (n_episodes - 1) % self.hparams["video"] == 0:
                         frames = np.stack([x["frame"] for x in replay])
